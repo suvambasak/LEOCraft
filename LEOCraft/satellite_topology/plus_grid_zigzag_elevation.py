a20dbf9486f618d02fd9982067c898d5589b199a
Suvam Basak <suvambasak2008@gmail.com>
2024-05-22 18:48:48 +0530

    Inter shell ISL connection

--- LEOCraft/satellite_topology/plus_grid_zigzag_elevation.py
+++ LEOCraft/satellite_topology/plus_grid_zigzag_elevation.py
@@ -1,21 +1,17 @@
 from LEOCraft.satellite import LEOSatellite
-from LEOCraft.satellite_topology.LEO_sat_topology import LEOSatelliteTopology
+from LEOCraft.satellite_topology.plus_grid_shell import PlusGridShell
 
 
-class PlusGridShell(LEOSatelliteTopology):
+class PlusGridZigzagElevation(PlusGridShell):
     """
-    Implements +Grid topology in a shell.
-    Extendes LEOSatelliteTopology class
+    Extendes PlusGridShell class and implements +Grid topology in a shell
+    where consecutive orbits are kept in elevation difference of 5 to 10 km
+    forming Zig Zag east to west (inter-orbit).
 
     - Generates TLE of all the satellite in a shell
     - Export TLEs into a file
     - Generates ISL links (sat_1, sat_2)
     - Export ISLs into a CSV file
-
-
-    Reference:
-    [1] https://github.com/snkas/hypatia/tree/master/satgenpy
-    [2] Kassing S, Bhattacherjee D, Águas AB, Saethre JE, Singla A. Exploring the" Internet from space" with Hypatia. InProceedings of the ACM Internet Measurement conference 2020 Oct 27 (pp. 214-229).
     """
 
     def __init__(
@@ -24,7 +20,7 @@ class PlusGridShell(LEOSatelliteTopology):
         orbits: int,
         sat_per_orbit: int,
 
-        altitude_m: float,
+        altitude_pattern_m: list[float],
         inclination_degree: float,
         angle_of_elevation_degree: float,
         phase_offset: float,
@@ -39,33 +35,38 @@ class PlusGridShell(LEOSatelliteTopology):
             Number of orbits
         sat_per_orbit: int
             Number of satellites per orbit
-        altitudes_m : float
-            Satellite altitude in meter(s)
+        altitude_pattern_m : list[float]
+            List of consecutive orbit's altitude pattern in meter(s)
         inclination_degree : float
             Angle of inclination in degree
         angle_of_elevation_degree:
             Min angle of elevation in degree
+
         phase_offset: float: float
             Offset between satellite of adjacent orbit
         """
+
+        assert len(altitude_pattern_m) > 1
 
         super().__init__(
             id=id,
             orbits=orbits,
             sat_per_orbit=sat_per_orbit,
 
+            altitude_m=altitude_pattern_m[0],
             inclination_degree=inclination_degree,
             angle_of_elevation_degree=angle_of_elevation_degree,
             phase_offset=phase_offset
         )
 
-        self.altitude_m = altitude_m
+        self.altitude_pattern_m = altitude_pattern_m
 
     def build_satellites(self) -> None:
         "Build TLEs and set epoch time satellites of this shell"
 
         satellite_counter = 0
 
+        altitude_index = 0
         for orbit in range(0, self.orbits):
 
             # Orbit across longitude
@@ -84,7 +85,9 @@ class PlusGridShell(LEOSatelliteTopology):
 
                 # Creating satellites
                 _leosat = LEOSatellite(
-                    altitude_m=self.altitude_m,
+                    altitude_m=self.altitude_pattern_m[
+                        orbit % len(self.altitude_pattern_m)
+                    ],
                     inclination_degree=self.inclination_degree,
                     angle_of_elevation_degree=self.angle_of_elevation_degree,
                     satellite_catalog_number=satellite_counter+1,
@@ -109,29 +112,6 @@ class PlusGridShell(LEOSatelliteTopology):
 
                 satellite_counter += 1
 
-    def build_ISLs(self) -> None:
-        "Generates ISL links (sat_1, sat_2)"
-
-        for i in range(self.orbits):
-            for j in range(self.sat_per_orbit):
-                sat = i * self.sat_per_orbit + j
-
-                # Link to the next in the orbit
-                sat_same_orbit = i * self.sat_per_orbit + \
-                    ((j + 1) % self.sat_per_orbit)
-                sat_adjacent_orbit = (
-                    (i + 1) % self.orbits
-                ) * self.sat_per_orbit + (j % self.sat_per_orbit)
-
-                # Same orbit
-                self.isls.add(
-                    (min(sat, sat_same_orbit), max(sat, sat_same_orbit))
-                )
-                # Adjacent orbit
-                self.isls.add(
-                    (min(sat, sat_adjacent_orbit), max(sat, sat_adjacent_orbit))
-                )
-
     @property
     def filename(self) -> str:
         """Generates file name from orbital parameters
@@ -139,6 +119,7 @@ class PlusGridShell(LEOSatelliteTopology):
         Returns
         -------
         str
-            Filename 
+            File name 
         """
-        return f'{self.__class__.__name__}_{self.id}_o{self.orbits}n{self.sat_per_orbit}h{self.altitude_m}i{self.inclination_degree}e{self.angle_of_elevation_degree}p{self.phase_offset}'
+
+        return f"{self.__class__.__name__}_{self.id}_o{self.orbits}n{self.sat_per_orbit}h{'-'.join([str(h) for h in self.altitude_pattern_m])}i{self.inclination_degree}e{self.angle_of_elevation_degree}p{self.phase_offset}"
