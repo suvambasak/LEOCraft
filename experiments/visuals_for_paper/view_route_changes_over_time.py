ba4771e76f50ecbf4b54b8846433d5d587dde819
Suvam Basak <suvambasak2008@gmail.com>
2025-05-01 19:18:07 +0530

    Add script to illustrates LEO dynamic and route changes between two ground stations over time.

---
+++ experiments/visuals_for_paper/view_route_changes_over_time.py
@@ -0,0 +1,170 @@
+
+'''
+This script illustrates LEO dynamic and route changes between two ground stations over time.
+Renders 3D view of frame by frame with 1 seconds of granularity.
+'''
+
+import time
+
+from LEOCraft.constellations.LEO_constellation import LEOConstellation
+from LEOCraft.dataset import GroundStationAtCities
+from LEOCraft.satellite_topology.plus_grid_shell import PlusGridShell
+from LEOCraft.user_terminals.ground_station import GroundStation
+from LEOCraft.utilities import k_shortest_paths
+from LEOCraft.visuals.sat_view_3D import SatView3D
+
+OUTPUT_DIR = './route_changes'
+
+c = 0
+
+start_time = time.perf_counter()
+
+
+for t in range(0, 1500, 1):
+    leo_con = LEOConstellation('Starlink')
+    leo_con.v.verbose = True
+    leo_con.add_ground_stations(
+        GroundStation(
+            GroundStationAtCities.TOP_100
+            # GroundStationAtCities.TOP_1000
+        )
+    )
+
+    # leo_con.add_shells(
+    #     PlusGridShell(
+    #         id=0,
+    #         # orbits=324,
+    #         orbits=24,
+    #         sat_per_orbit=12,
+    #         altitude_m=550000.0,
+    #         inclination_degree=45.3,
+    #         angle_of_elevation_degree=16.4,
+    #         phase_offset=50.0
+    #     )
+    # )
+
+    # leo_con.add_shells(
+    #     PlusGridShell(
+    #         id=0,
+    #         orbits=144,
+    #         sat_per_orbit=11,
+    #         altitude_m=550000.0,
+    #         inclination_degree=38.6,
+    #         angle_of_elevation_degree=12.1,
+    #         # angle_of_elevation_degree=20.0,
+    #         phase_offset=50.0
+    #     )
+    # )
+
+    # # Adding Shells
+    # Starlink Shell 1
+    leo_con.add_shells(
+        PlusGridShell(
+            id=0,
+            orbits=72,
+            sat_per_orbit=22,
+            altitude_m=550000.0,
+            inclination_degree=53.0,
+            angle_of_elevation_degree=25.0,
+            phase_offset=50.0
+        )
+    )
+
+    # # Starlink Shell 2
+    # leo_con.add_shells(
+    #     PlusGridShell(
+    #         id=1,
+    #         orbits=72,
+    #         sat_per_orbit=22,
+    #         altitude_m=540000.0,
+    #         inclination_degree=53.2,
+    #         angle_of_elevation_degree=25.0,
+    #         phase_offset=50.0
+    #     )
+    # )
+
+    # # Starlink Shell 3
+    # leo_con.add_shells(
+    #     PlusGridShell(
+    #         id=2,
+    #         orbits=36,
+    #         sat_per_orbit=20,
+    #         altitude_m=570000.0,
+    #         inclination_degree=70.0,
+    #         angle_of_elevation_degree=25.0,
+    #         phase_offset=50.0
+    #     )
+    # )
+
+    leo_con.set_time(second=t)  # Time passed after epoch
+    leo_con.set_loss_model(None)
+    leo_con.build()
+    leo_con.create_network_graph()
+
+    sat_info = leo_con.sat_info('S0-1')
+
+    # leo_con.generate_routes()
+
+    src = 'G-0'
+    dst = 'G-36'
+
+    leo_con.connect_ground_station(src, dst)
+    compute_status, flow, k_path = k_shortest_paths(
+        leo_con.sat_net_graph, src, dst, k=20
+    )
+
+    leo_con.routes = dict()
+    leo_con.link_load = dict()
+    leo_con.no_path_found = set()
+    leo_con.k_path_not_found = set()
+
+    leo_con._add_route(compute_status, flow, k_path)
+
+    view = SatView3D(
+        leo_con,
+        # lat=sat_info.nadir_latitude,
+        lat=16.80528,
+        # long=sat_info.nadir_longitude,
+        long=96.15611,
+        title=f'Time: {t}s'
+    )
+    # view.add_ground_stations('G-0', 'G-1', 'G-2', 'G-3')
+    # view.add_satellites('S0-0', 'S0-1', 'S0-2', 'S0-3', 'S0-26')
+    # view.add_coverages('S0-0', 'S0-1', 'S0-40', 'S0-31', 'S0-30')
+    # view.add_GSLs('G-1', 'G-2')
+    # view.add_ISLs((
+    #     ('S0-0', 'S0-1'), ('S0-1', 'S0-2'),
+    #     ('S0-12', 'S0-13'), ('S0-13', 'S0-14'),
+    #     ('S0-0', 'S0-12'), ('S0-1', 'S0-13'), ('S0-2', 'S0-14')
+    # ))
+    # view.add_routes('G-0_G-1', 'G-1_G-2', 'G-2_G-3', 'G-30_G-33', k=1)
+    # view.add_all_ISLs()
+    view.add_routes(flow, k=1)
+
+    # view.add_all_ground_stations()
+    view.add_all_satellites()
+    # view.add_all_coverages()
+    # view.add_all_GSLs()
+    # view.add_all_ISLs()
+    # view.add_all_routes()
+
+    # view.highlight_satellites(
+    #     ['S0-0', 'S0-1', 'S0-2'])
+    # if c % 2:
+    #     view.highlight_satellites(
+    #         ['S0-0', 'S0-1', 'S0-2'])
+    # else:
+    #     view.highlight_satellites(
+    #         ['S0-0', 'S0-1', 'S0-2', 'S0-12', 'S0-13', 'S0-14'])
+
+    c += 1
+
+    view.build()
+    # view.show()
+
+    # view.export_html()
+    view.export_png(f'{OUTPUT_DIR}/{str(t).zfill(4)}.png')
+
+
+end_time = time.perf_counter()
+print(f'Total simulation time: {round((end_time-start_time)/60, 2)}m')
