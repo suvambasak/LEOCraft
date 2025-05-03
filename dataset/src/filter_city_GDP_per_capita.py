444f955ba0e4172c577569bf4e9b6e1e717e027a
Suvam Basak <suvambasak2008@gmail.com>
2025-05-03 19:34:34 +0530

    Prepare the dataset for traffic metrics

---
+++ dataset/src/filter_city_GDP_per_capita.py
@@ -0,0 +1,65 @@
+
+'''
+Find the GDP per capita of a city by its name,
+Keep the city name and GDP per capita in a CSV file empty when not found.
+'''
+
+import pandas as pd
+
+
+def find_GDP_per_capita_by_city_name():
+    'Find the GDP per capita of a city by its name'
+
+    df_few_cities = pd.read_csv(CITIES_CSV)
+    df_few_cities['name'] = df_few_cities['name'].str.lower()
+
+    df_all_GDP = pd.read_csv(ALL_GDP_CSV)
+    CITY_NAME_LABEL = df_all_GDP.columns[1]
+    GDP_PER_CAPITA_LABEL = df_all_GDP.columns[-1]
+    df_all_GDP[CITY_NAME_LABEL] = df_all_GDP[CITY_NAME_LABEL].str.lower()
+
+    # Record the population of the cities
+    GDP_per_capita_dataset = list()
+
+    for row in df_few_cities.itertuples(index=True, name='City'):
+
+        # Create a record for each city
+        GDP_per_capita_record = {'name_1': row.name}
+
+        result = df_all_GDP.loc[
+            df_all_GDP[CITY_NAME_LABEL] == row.name
+        ]
+
+        if not result.empty:
+            GDP_per_capita_record['name_2'] = result.iloc[0][CITY_NAME_LABEL]
+            GDP_per_capita_record['GDP_per_capita'] = float(result.iloc[0][GDP_PER_CAPITA_LABEL].replace(
+                ',', ''))
+            print(
+                f'|- {row.name}:{result.iloc[0][CITY_NAME_LABEL]} {result.iloc[0][GDP_PER_CAPITA_LABEL]}'
+            )
+
+        else:
+            GDP_per_capita_record['name_2'] = None
+            GDP_per_capita_record['GDP_per_capita'] = 'MANUALLY_SET'
+            print(
+                f'|x {row.name}: Not Found'
+            )
+
+        GDP_per_capita_dataset.append(GDP_per_capita_record)
+
+    pd.DataFrame(
+        GDP_per_capita_dataset
+    ).to_csv(GDP_PER_CAPITA_OF_CITIES_CSV, index=False)
+
+
+if __name__ == '__main__':
+
+    ALL_GDP_CSV = 'dataset/src/CSVs/List_of_cities_by_GDP_2.csv'
+
+    # CITIES_CSV = 'dataset/ground_stations/cities_sorted_by_estimated_2025_pop_top_100.csv'
+    # GDP_PER_CAPITA_OF_CITIES_CSV = 'dataset/src/CSVs/GDP_per_capita_100.csv'
+
+    CITIES_CSV = 'dataset/ground_stations/cities_sorted_by_estimated_2025_pop_top_1000.csv'
+    GDP_PER_CAPITA_OF_CITIES_CSV = 'dataset/src/CSVs/GDP_per_capita_1000.csv'
+
+    find_GDP_per_capita_by_city_name()
---
+++ dataset/src/filter_city_population.py
@@ -0,0 +1,125 @@
+
+'''
+Filter the population of cities from the worldcities.csv dataset
+and find the closest city when name do not macth.
+'''
+
+import sys
+
+import pandas as pd
+from geopy.distance import great_circle
+
+
+def geodesic_distance_km(
+    lat_1: float,
+    lng_1: float,
+
+    lat_2: float,
+    lng_2: float
+) -> float:
+    'Calculate the geodesic distance in kilometers between two points on the Earth'
+
+    EARTH_RADIOUS_KM = 6378.135
+
+    return great_circle(
+        (float(lat_1), float(lng_1)),
+        (float(lat_2), float(lng_2)),
+        radius=EARTH_RADIOUS_KM
+    ).km
+
+
+def find_closest_city(city_info, df_all_cities: pd.DataFrame) -> tuple[str, float, float]:
+    'Find the closest city to the given city information in the DataFrame'
+
+    min_distance = sys.maxsize
+    closest_city = None
+
+    for row in df_all_cities.itertuples(index=True, name='City'):
+
+        current_distance = geodesic_distance_km(
+            city_info.latitude_degree,
+            city_info.longitude_degree,
+            row.lat,
+            row.lng
+        )
+
+        if current_distance < min_distance:
+            min_distance = current_distance
+            closest_city = row
+
+    return closest_city.city, closest_city.population, min_distance
+
+
+def find_population_by_city_name():
+    'Find the population of a city by its name in the DataFrame'
+
+    df_all_cities = pd.read_csv(ALL_CITIES_CSV)
+    df_few_cities = pd.read_csv(FEW_CITIES_CSV)
+
+    # Convert the city names to lowercase for case-insensitive comparison
+    df_all_cities['lcity'] = df_all_cities['city'].str.lower()
+    df_all_cities['lcity_ascii'] = df_all_cities['city_ascii'].str.lower()
+    df_few_cities['lname'] = df_few_cities['name'].str.lower()
+
+    # Record the population of the cities
+    population_dataset = list()
+
+    for row in df_few_cities.itertuples(index=True, name='City'):
+
+        # Find by city name and city name ascii
+        result_city = df_all_cities.loc[
+            df_all_cities['lcity'] == row.lname
+        ]
+        result_city_ascii = df_all_cities.loc[
+            df_all_cities['lcity_ascii'] == row.lname
+        ]
+
+        population_record = {'name_1': row.name, 'distance_km': 0.0}
+
+        if not result_city.empty:
+            population_record['name_2'] = result_city.iloc[0]['city']
+            population_record['population'] = str(
+                int(result_city.iloc[0]['population'])
+            )
+            print(
+                f'|- {row.name}:{result_city.iloc[0]["city"]} {result_city.iloc[0]["population"]}'
+            )
+
+        elif not result_city_ascii.empty:
+            population_record['name_2'] = result_city_ascii.iloc[0]['city_ascii']
+            population_record['population'] = str(
+                int(result_city_ascii.iloc[0]['population'])
+            )
+            print(
+                f'|- {row.name}:{result_city_ascii.iloc[0]["city_ascii"]} {result_city_ascii.iloc[0]["population"]}'
+            )
+
+        else:
+            city_name, city_population, min_distance = find_closest_city(
+                row, df_all_cities
+            )
+            population_record['name_2'] = city_name
+            population_record['population'] = str(int(city_population))
+            population_record['distance_km'] = 0.0
+            print(
+                f'|x {row.name}: [closest] : {city_name} {city_population} ## {min_distance:.2f} km'
+            )
+
+        population_dataset.append(population_record)
+
+    pd.DataFrame(
+        population_dataset
+    ).to_csv(POPULATION_CSV, index=False)
+
+
+if __name__ == '__main__':
+
+    ALL_CITIES_CSV = 'dataset/src/CSVs/worldcities.csv'
+
+    FEW_CITIES_CSV = 'dataset/ground_stations/cities_sorted_by_estimated_2025_pop_top_100.csv'
+    POPULATION_CSV = 'dataset/src/CSVs/population_100.csv'
+
+    # FEW_CITIES_CSV = 'dataset/ground_stations/cities_sorted_by_estimated_2025_pop_top_1000.csv'
+    # POPULATION_CSV = 'dataset/src/CSVs/population_1000.csv'
+
+    find_population_by_city_name()
