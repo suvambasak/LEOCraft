95ebe6b003e99aebc403d3391bcff0fa05fe1a47
Suvam Basak <suvambasak2008@gmail.com>
2025-05-06 09:23:45 +0530

    Fix the paramters name error.

--- experiments/multi_shell_design/throughput_shift_inter_shell_ISL_without_handoff_h24.py
+++ experiments/multi_shell_design/throughput_shift_inter_shell_ISL_without_handoff_h24.py
@@ -54,34 +54,34 @@ def simulate_h24(params):
         if params.topology_type == 'SS_ISL':
             shell = PlusGridShell(
                 id=0,
-                orbits=params.o,
-                sat_per_orbit=params.n,
-                phase_offset=params.p,
-                altitude_m=params.h*1000,
-                angle_of_elevation_degree=params.e,
-                inclination_degree=params.i
+                orbits=params.S0_o,
+                sat_per_orbit=params.S0_n,
+                phase_offset=params.S0_p,
+                altitude_m=params.S0_h_km*1000,
+                angle_of_elevation_degree=params.S0_e,
+                inclination_degree=params.S0_i
             )
         elif params.topology_type == 'IS2_ISL':
             shell = PlusGridZigzagElevation(
                 id=0,
-                orbits=params.o,
-                sat_per_orbit=params.n,
+                orbits=params.S0_o,
+                sat_per_orbit=params.S0_n,
                 altitude_pattern_m=[540000.0, 550000.0],
-                inclination_degree=params.i,
-                angle_of_elevation_degree=params.e,
-                phase_offset=params.p
+                inclination_degree=params.S0_i,
+                angle_of_elevation_degree=params.S0_e,
+                phase_offset=params.S0_p
             )
         elif params.topology_type == 'IS3_ISL':
             shell = PlusGridZigzagElevation(
                 id=0,
-                orbits=params.o,
-                sat_per_orbit=params.n,
+                orbits=params.S0_o,
+                sat_per_orbit=params.S0_n,
                 altitude_pattern_m=[
                     540000.0, 550000.0, 570000.0, 550000.0
                 ],
-                inclination_degree=params.i,
-                angle_of_elevation_degree=params.e,
-                phase_offset=params.p
+                inclination_degree=params.S0_i,
+                angle_of_elevation_degree=params.S0_e,
+                phase_offset=params.S0_p
             )
 
         leo_con.add_shells(shell)
